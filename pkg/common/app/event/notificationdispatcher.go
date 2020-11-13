ba07bb30b29d5e6b98e17b390eb526185934e3b8
Viacheslav Bartsev <klwxsrx@icloud.com>
2020-11-13 23:01:54 +0300

    Добавил диспатчер уведомлений новых доменных событий

---
+++ pkg/common/app/event/notificationdispatcher.go
@@ -0,0 +1,66 @@
+package event
+
+import (
+	"github.com/klwxsrx/expense-tracker/pkg/common/app/logger"
+	"github.com/klwxsrx/expense-tracker/pkg/common/infrastructure/event/messaging"
+	"sync/atomic"
+	"time"
+)
+
+const dispatchPeriod = time.Millisecond * 500
+
+type StoredEventNotificationDispatcher interface {
+	Dispatch()
+	Stop()
+}
+
+type notificationDispatcher struct {
+	notifier     messaging.StoredEventNotifier
+	logger       logger.Logger
+	needDispatch int32
+	stopChan     chan struct{}
+}
+
+func (d *notificationDispatcher) Dispatch() {
+	atomic.StoreInt32(&d.needDispatch, 1)
+}
+
+func (d *notificationDispatcher) Stop() {
+	d.stopChan <- struct{}{}
+}
+
+func (d *notificationDispatcher) start() {
+	errorsChan := make(chan error)
+	go func() {
+		for {
+			select {
+			case err := <-errorsChan:
+				d.logger.With(logger.Fields{"error": err}).Error("failed to dispatch event notification")
+			}
+		}
+	}()
+
+	ticker := time.NewTicker(dispatchPeriod)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				needDispatch := atomic.SwapInt32(&d.needDispatch, 0)
+				if needDispatch == 1 {
+					err := d.notifier.NotifyOfCreatedEvents()
+					if err != nil {
+						errorsChan <- err
+					}
+				}
+			case <-d.stopChan:
+				return
+			}
+		}
+	}()
+}
+
+func NewStoredEventNotificationDispatcher(notifier messaging.StoredEventNotifier, logger logger.Logger) StoredEventNotificationDispatcher {
+	dispatcher := &notificationDispatcher{notifier, logger, 1, make(chan struct{})}
+	dispatcher.start()
+	return dispatcher
+}
--- pkg/common/infrastructure/event/messaging/eventnotifier.go
+++ pkg/common/infrastructure/event/messaging/eventnotifier.go
@@ -14,13 +14,17 @@ const (
 	messageContentType = "text/plain"
 )
 
-type StoredEventNotifier struct {
+type StoredEventNotifier interface {
+	NotifyOfCreatedEvents() error
+}
+
+type storedEventNotifier struct {
 	store   app.Store
 	client  mysql.Client
 	channel *amqp.Channel
 }
 
-func (en *StoredEventNotifier) NotifyOfCreatedEvents() error {
+func (en *storedEventNotifier) NotifyOfCreatedEvents() error {
 	lock := mysql.NewLock(en.client, "stored_event_notification")
 	err := lock.Get()
 	if err != nil {
@@ -45,7 +49,7 @@ func (en *StoredEventNotifier) NotifyOfCreatedEvents() error {
 	return en.updateLastSentEventID(lastSentID)
 }
 
-func (en *StoredEventNotifier) Connect(connection *amqp.Connection) error {
+func (en *storedEventNotifier) Connect(connection *amqp.Connection) error {
 	ch, err := connection.Channel()
 	if err != nil {
 		return err
@@ -58,18 +62,18 @@ func (en *StoredEventNotifier) Connect(connection *amqp.Connection) error {
 	return nil
 }
 
-func (en *StoredEventNotifier) getLastSentEventID() (app.StoredEventID, error) {
+func (en *storedEventNotifier) getLastSentEventID() (app.StoredEventID, error) {
 	var id app.StoredEventID
 	err := en.client.Get(&id, "SELECT id FROM last_notified_event LIMIT 1")
 	return id, err
 }
 
-func (en *StoredEventNotifier) updateLastSentEventID(id app.StoredEventID) error {
+func (en *storedEventNotifier) updateLastSentEventID(id app.StoredEventID) error {
 	_, err := en.client.Exec("UPDATE last_notified_event SET id = ?", id)
 	return err
 }
 
-func (en *StoredEventNotifier) sendNotification(id app.StoredEventID) error {
+func (en *storedEventNotifier) sendNotification(id app.StoredEventID) error {
 	msg := amqp.Publishing{
 		DeliveryMode: amqp.Persistent,
 		ContentType:  messageContentType,
@@ -78,6 +82,6 @@ func (en *StoredEventNotifier) sendNotification(id app.StoredEventID) error {
 	return en.channel.Publish(exchangeName, routingKey, false, false, msg)
 }
 
-func NewStoredEventNotifier(store app.Store, client mysql.Client, channel *amqp.Channel) *StoredEventNotifier {
-	return &StoredEventNotifier{store, client, channel}
+func NewStoredEventNotifier(store app.Store, client mysql.Client, channel *amqp.Channel) StoredEventNotifier {
+	return &storedEventNotifier{store, client, channel}
 }
